ad5953100596954210203a525d62eb365a999c3b
joaocandidozup <joao.candido@zup.com>
2024-12-04 09:26:09 -0300

    feat:add application business rules

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("########## Calculadora do joão ##########");
         double numero1 = 4;
-        double numero2 = 3;
+        double numero2 = 0;
         /*
         operações:
         1 - soma
@@ -10,8 +10,23 @@ public class Main {
         3 - multiplicação
         4 - divisão
         */
-        int operacao = 0;
+        int operacao = 4;
 
+        if (operacao == 1) {
+            System.out.printf("O resultado de (%.1f + %.1f) = " + (numero1 + numero2), numero1, numero2);
+        } else if (operacao == 2) {
+            System.out.printf("O resultado de (%.1f - %.1f) = " + (numero1 - numero2), numero1, numero2);
+        } else if (operacao == 3) {
+            System.out.printf("O resultado de (%.1f x %.1f) = " + (numero1 * numero2), numero1, numero2);
+        }else if (operacao == 4) {
+            if (numero2 != 0){
+                System.out.printf("O resultado de (%.1f / %.1f) = " + (numero1 / numero2), numero1, numero2);
+            }else {
+                System.out.printf("o resultado de (%.1f / %.1f) é uma operação matematica indefinida!!!",numero1,numero2);
+            }
+        }else{
+            System.out.println("operação invalida!!!");
+        }
 
     }
 }
