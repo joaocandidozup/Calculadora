7546cac9e5351b48c4ea99a136dd848bfe8e5f30
joaocandidozup <joao.candido@zup.com>
2024-12-04 09:04:53 -0300

    feat:add context variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("########## Calculadora do joão ##########");
+        double numero1 = 4;
+        double numero2 = 3;
+        /*
+        operações:
+        1 - soma
+        2 - subtração
+        3 - multiplicação
+        4 - divisão
+        */
+        int operacao = 0;
+
+
     }
 }
